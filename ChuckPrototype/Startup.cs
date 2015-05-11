5707b210687c40944cba6d678726f78bc1c97d6d
hanislovingit <hanislovingit@gmail.com>
2015-05-11 16:17:55 -0500

    Initial commmit. Default asp.net mvc project.

---
+++ ChuckPrototype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChuckPrototype.Startup))]
+namespace ChuckPrototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
